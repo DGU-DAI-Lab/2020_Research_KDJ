4996f5ad4ff97002f14ae730d847d47cee74db02
Hepheir <hepheir@gmail.com>
2020-06-04 14:06:09 +0900

    Replace wrong url

--- 200604_첨성대벽돌균열/util.py
+++ 200604_첨성대벽돌균열/util.py
@@ -126,7 +126,7 @@ class DG_FileSystem:
 
     def imread(filename, flags=cv2.IMREAD_COLOR, dtype=np.uint8):
         """유니코드로 이루어진 파일경로를 읽기위한 함수. 동작은 cv2.imread()와 동일하다.\n
-        scrapped from: https://cjh5414.github.io/python-with/"""
+        scrapped from: https://jangjy.tistory.com/337"""
         try:
             n = np.fromfile(filename, dtype)
             img = cv2.imdecode(n, flags)
@@ -137,7 +137,7 @@ class DG_FileSystem:
 
     def imwrite(filename, img, params=None):
         """유니코드로 이루어진 파일경로를 읽기위한 함수. 동작은 cv2.imwrite()와 동일하다.\n
-        scrapped from: https://cjh5414.github.io/python-with/"""
+        scrapped from: https://jangjy.tistory.com/337"""
         try:
             ext = os.path.splitext(filename)[1]
             result, n = cv2.imencode(ext, img, params)
